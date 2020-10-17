ead036cec7f5ac92deca241641bf555ec7ab42e1
Balázs Orbán <info@balazsorban.com>
2020-10-17 12:24:35 +0200

    simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testURL: "http://localhost/",
-  collectCoverageFrom: [
-    "src/**/*.ts"
-  ],
   moduleDirectories: ['node_modules', 'src']
 };
